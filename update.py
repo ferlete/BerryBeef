dfb779c7eae76a49f89508f4f10d4d59f1bda31d
Valter Ferlete <ferlete@gmail.com>
2020-04-11 10:24:33 -0400

    Initial version

--- update.py
+++ update.py
@@ -1,15 +1,13 @@
 import git
-changed_files = []
 
-#https://www.devdungeon.com/content/working-git-repositories-python
+# https://www.devdungeon.com/content/working-git-repositories-python
 if __name__ == "__main__":
     # so isto daqui atualiza repositorio local
-    #g = git.Git('/home/ferlete/gitAAA/')
-    # g.pull('origin', 'master')
+    # g = git.Git('/home/ferlete/gitAAA/')
+    # g.pull('origin', 'origin/master')
 
     repo = git.Repo("/home/ferlete/gitAAA/")
-    if repo.is_dirty(untracked_files=True):
-       print('Changes detected.')
-
-
-
+if repo.is_dirty(untracked_files=True):
+    print('Changes detected.')
+else:
+    print('not changes detected')
