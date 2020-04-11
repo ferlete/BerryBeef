4003e853b0d056bfe138dd195d79a9bad70900e9
Valter Ferlete <ferlete@gmail.com>
2020-04-11 09:17:18 -0400

    Initial version

--- read_repo.py
+++ read_repo.py
@@ -2,11 +2,5 @@ import git
 import os
 
 if __name__ == "__main__":
-   #repo = git.Repo('/home/ferlete/PycharmProjects/BERRYBEEF/')
-   #repo = git.Repo.clone_from("https://github.com/ferlete/BERRYBEEF", os.path.join('/home/ferlete/PycharmProjects/', 'BERRYBEEF'), branch='master')
-   #origin = repo.remotes.origin
-   #origin.pull()
-   #print(origin)
-   g = git.Git('/home/ferlete/PycharmProjects/BERRYBEEF/')
-   print(g.version_info)
-   g.pull('origin', 'master')
+   g = git.Git('/home/ferlete/gitAAA/')
+   g.pull('origin', 'origin/master')
