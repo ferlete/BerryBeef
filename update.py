34d5937b8794b3b1d5a42c78a27d84574bb8a7f4
Valter Ferlete <ferlete@gmail.com>
2020-04-12 06:38:32 -0400

    Initial version

--- update.py
+++ update.py
@@ -1,21 +1,18 @@
 import git
-from urllib.parse import urlparse
 
 # https://www.devdungeon.com/content/working-git-repositories-python
 if __name__ == "__main__":
     # so isto daqui atualiza repositorio local
-    # g = git.Git('/home/ferlete/gitAAA/')
-    # g.pull('origin', 'origin/master')
+    g = git.Git('/home/ferlete/gitAAA/')
+    g.pull('origin', 'origin/master')
 
-    repo = git.Repo("/home/ferlete/gitAAA/")
-    heads = repo.heads
-    print(heads)
-    headcommit = repo.head.commit
-    import time
-    time.asctime(time.gmtime(headcommit.committed_date))
-    print(time.strftime("%a,%d%b %Y %H:%M", time.gmtime(headcommit.committed_date)))
-    print(headcommit.diff('version.txt'))
+    # repo = git.Repo("/home/ferlete/gitAAA/")
+    # heads = repo.heads
+    # print(heads)
+    # headcommit = repo.head.commit
+    # import time
+    # time.asctime(time.gmtime(headcommit.committed_date))
+    # print(time.strftime("%a,%d%b %Y %H:%M", time.gmtime(headcommit.committed_date)))
 
 
-    parsed = urlparse('http://netloc/path;parameters?query=argument#fragment')
-    print(parsed)
+
