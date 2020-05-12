3a7d6d4bf1c6d68fde4bdcccf24bdb977b7f2417
ferlete@gmail.com <GithubVltrvof123!>
2020-05-12 19:29:07 -0400

    Berry Beef 0.1

--- BERRYBEEF/Dialog_Real_Time_UI.py
+++ BERRYBEEF/Dialog_Real_Time_UI.py
@@ -2,23 +2,25 @@
 
 # Form implementation generated from reading ui file 'Dialog_Real_Time_UI.ui'
 #
-# Created by: PyQt5 UI code generator 5.7
+# Created by: PyQt5 UI code generator 5.14.1
 #
 # WARNING! All changes made in this file will be lost!
 
+
 from PyQt5 import QtCore, QtGui, QtWidgets
+
 
 class Ui_Dialog(object):
     def setupUi(self, Dialog):
         Dialog.setObjectName("Dialog")
         Dialog.resize(468, 436)
         self.buttonBox = QtWidgets.QDialogButtonBox(Dialog)
-        self.buttonBox.setGeometry(QtCore.QRect(360, 380, 81, 32))
+        self.buttonBox.setGeometry(QtCore.QRect(180, 380, 81, 32))
         self.buttonBox.setOrientation(QtCore.Qt.Horizontal)
         self.buttonBox.setStandardButtons(QtWidgets.QDialogButtonBox.Ok)
         self.buttonBox.setObjectName("buttonBox")
         self.lbl_result = QtWidgets.QLabel(Dialog)
-        self.lbl_result.setGeometry(QtCore.QRect(20, 20, 441, 351))
+        self.lbl_result.setGeometry(QtCore.QRect(20, 20, 441, 261))
         font = QtGui.QFont()
         font.setPointSize(22)
         font.setBold(True)
@@ -26,6 +28,12 @@ class Ui_Dialog(object):
         font.setWeight(75)
         self.lbl_result.setFont(font)
         self.lbl_result.setObjectName("lbl_result")
+        self.lbl_apro = QtWidgets.QLabel(Dialog)
+        self.lbl_apro.setGeometry(QtCore.QRect(10, 310, 441, 61))
+        font = QtGui.QFont()
+        font.setPointSize(48)
+        self.lbl_apro.setFont(font)
+        self.lbl_apro.setObjectName("lbl_apro")
 
         self.retranslateUi(Dialog)
         self.buttonBox.accepted.connect(Dialog.accept)
@@ -36,4 +44,4 @@ class Ui_Dialog(object):
         _translate = QtCore.QCoreApplication.translate
         Dialog.setWindowTitle(_translate("Dialog", "Predição em tempo real"))
         self.lbl_result.setText(_translate("Dialog", "TextLabel"))
-
+        self.lbl_apro.setText(_translate("Dialog", "TextLabel"))
--- BERRYBEEF/Main.py
+++ BERRYBEEF/Main.py
@@ -2,7 +2,7 @@ import sys
 
 from PyQt5 import QtCore, QtWidgets, QtGui
 from PyQt5.QtGui import QIcon
-from PyQt5.QtWidgets import QMainWindow, QAction, QMessageBox,  QFileDialog
+from PyQt5.QtWidgets import QMainWindow, QAction, QMessageBox, QFileDialog
 from configparser import ConfigParser
 
 from sklearn.cross_decomposition import PLSRegression
@@ -100,7 +100,7 @@ class MainWindow(QMainWindow):
         try:
 
             file = FileIO()
-            config_file = DB_FOLDER + "/config.ini"
+            config_file = DB_FOLDER + self.project_selected + "/config.ini"
 
             if file.file_exists(config_file):
                 trm_name, _ = QFileDialog.getOpenFileName(self, 'Open TRM File', 'TRM', 'TRM(*.TRM)')
@@ -108,7 +108,7 @@ class MainWindow(QMainWindow):
 
                     config = ConfigParser()
                     config.read(config_file)
-                    ph_model = DB_FOLDER + config.get("Model", "ph")
+                    ph_model = DB_FOLDER + self.project_selected + "/" + config.get("Model", "ph")
 
                     data_predict = file.load_trm_to_dataframe(trm_name)
                     data_model_ph = file.load_csv_pandas(ph_model, sep=';')
@@ -117,8 +117,12 @@ class MainWindow(QMainWindow):
                         QMessageBox.about(self, "Erro", 'Qtde de colunas do modelo deve ser a mesma da predição')
                     else:
 
-                        reference = DB_FOLDER + config.get("Reference", "ref")
+                        reference = DB_FOLDER + self.project_selected + "/" + config.get("Reference", "ref")
+                        quality = DB_FOLDER + self.project_selected + "/" + config.get("Parameter", "quality_file")
+
                         data_reference = file.load_csv_pandas(reference, sep=';')
+                        data_quality = file.load_csv_pandas(quality, sep=';')
+
                         X_valid = data_predict['Transmittance']
                         data_reference = data_reference.T
                         calc = Calcule()
@@ -128,30 +132,36 @@ class MainWindow(QMainWindow):
                         out = "Arquivo: " + os.path.basename(trm_name) + "\n\n"
 
                         for key, value in config.items('Model'):
-                            key_model = DB_FOLDER + config.get("Model", key)
-                            key_parameter = config.get("Parameter", key)
+                            key_model = DB_FOLDER + self.project_selected + "/" + config.get("Model", key)
                             data_model_key = file.load_csv_pandas(key_model, sep=';')
                             X_calib = data_model_key.iloc[:, 1:]
                             Y_calib = data_model_key.iloc[:, 0:1]  # apenas a coluna de predicao
                             pls = PLSRegression(n_components=2, max_iter=500)
                             pls.fit(X_calib, Y_calib)
                             Y_pred = pls.predict(X_valid)  # valores preditos
-                            if round(float(Y_pred[0]), 3) > float(key_parameter.replace(',', '.')):
-                                classifier = "A"
-                            else:
-                                classifier = "R"
 
-                            out = out + "{} = {} {}\n".format(key, round(float(Y_pred[0]), 3), classifier)
+                            # print(key, " Valor Predito:", round(float(Y_pred[0]), 1))
+                            query = 'Atributo == "' + key + '" & (de <= ' + str(round(float(Y_pred[0]), 3)) + ' <= ate)'
+                            rslt_df = data_quality.query(query)
+                            out = out + "{} = {} \n".format(key, rslt_df['Descricao'].iloc[0])
 
-                        out = out + "\nLegenda: \n A=Aprovado R=Reprovado"
+                            if (key == "ph"):
+                                valor_predito_ph = round(float(Y_pred[0]), 1)
+
+                        if (valor_predito_ph >= 5.3 and valor_predito_ph <= 5.8):
+                            apro = "Aprovado"
+                            style = "background-color: green; border: 1px solid black;"
+                        else:
+                            apro = "Reprovado"
+                            style = "background-color: red; border: 1px solid black;"
 
                         dialog = QtWidgets.QDialog()
                         self.dg_realtime = DRealTime()
                         self.dg_realtime.setupUi(dialog)
-
                         self.dg_realtime.lbl_result.setText(out)
-
-                        dialog.exec()
+                        self.dg_realtime.lbl_apro.setText(apro)
+                        self.dg_realtime.lbl_apro.setStyleSheet(style)
+                        dialog.exec_()
 
         except Exception as ex:
             print('Error on line {}'.format(sys.exc_info()[-1].tb_lineno), type(ex).__name__, ex)
